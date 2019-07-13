54e0c2f10c4a65434fac3fcd3d5d7a3fd0e1aeaf
daisyarusey <daisyarusey.dc@gmail.com>
2019-07-13 12:05:10 +0300

    Add array daOfWeek

--- js/script.js
+++ js/script.js
@@ -18,4 +18,5 @@ document.getElementById('theform').onsubmit = function generateName(){
   
   var maleName = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
   var femaleName = ["Akosu", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Amna"];
+  var dayOfWeek = ["Sunday", "Monday", "Tuesday", "Wensday", "Thursday", "Friday", "Sunday"];
 }
