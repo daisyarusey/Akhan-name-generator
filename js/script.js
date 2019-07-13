a43e30b915da0e296be1e98f557a4ee877c638d1
daisyarusey <daisyarusey.dc@gmail.com>
2019-07-13 11:57:00 +0300

    Get the gender element

--- js/script.js
+++ js/script.js
@@ -6,7 +6,15 @@ document.getElementById('theform').onsubmit = function generateName(){
   alert('Your date of birth is ' + date);
   var month = document.getElementById('month').value;
   alert('Your date of birth is ' + month);
+  var yearOfBirth = document.getElementById('year').value;
+  alert('Your date of birth is ' + yearOfBirth);
 
+  var century = yearOfBirth.slice(0,2);
+  var year = yearOfBirth.slice(2,4);
+
+  var genders = document.getElementById("gender");
+  var gender = genders.options[genders.selectedIndex].text;
+  alert(gender);
   
 
 }
