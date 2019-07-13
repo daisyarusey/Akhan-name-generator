ac364304d573190cec4f76227dfa1ac3e6e578dc
daisyarusey <daisyarusey.dc@gmail.com>
2019-07-13 12:10:35 +0300

    Add if loop

--- js/script.js
+++ js/script.js
@@ -5,9 +5,9 @@ document.getElementById('theform').onsubmit = function generateName(){
   var date = document.getElementById('date').value;
   alert('Your date of birth is ' + date);
   var month = document.getElementById('month').value;
-  alert('Your date of birth is ' + month);
+  alert('Your month of birth is ' + month);
   var yearOfBirth = document.getElementById('year').value;
-  alert('Your date of birth is ' + yearOfBirth);
+  alert('Your year of birth is ' + yearOfBirth);
 
   var century = yearOfBirth.slice(0,2);
   var year = yearOfBirth.slice(2,4);
@@ -20,5 +20,14 @@ document.getElementById('theform').onsubmit = function generateName(){
   var femaleName = ["Akosu", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Amna"];
   var dayOfWeek = ["Sunday", "Monday", "Tuesday", "Wensday", "Thursday", "Friday", "Sunday"];
   var dayOfBirth = parseInt(((century / 4) - 2 * century - 1) + ((5 * year / 4)) + ((26 * (month + 1) / 10))) % 7;
-
+  if (gender === "Male") {
+    var mName = maleName[dayOfBirth - 1]
+    var day = dayOfWeek[dayOfBirth -1]
+    alert(" Since you were born on " + day + " Your name is "+ mName);
+    }
+    else if (gender === "Female") {
+        var fName = femaleName[dayOfBirth -1]
+        var day = dayOfWeek[dayOfBirth - 1]
+        alert(" Since you were born on " + day + " Your name is "+ fName);
+    }
 }
