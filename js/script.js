889b4190ee638945e34855e9033f5ddfb791c5cd
daisyarusey <daisyarusey.dc@gmail.com>
2019-07-13 11:31:11 +0300

    Get the date element

--- js/script.js
+++ js/script.js
@@ -2,4 +2,7 @@ var century, year, month, date;
 
 document.getElementById('theform').onsubmit = function generateName(){
   console.log(document.getElementById('theform').value);
+  var dob = document.getElementById('dob').value;
+  alert('Your date of birth is ' + dob);
+  
 }
