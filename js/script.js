ded1db4e774118a4f4df2f974ea6bd246d810965
daisyarusey <daisyarusey.dc@gmail.com>
2019-07-14 19:30:08 +0300

    Edit the arrangement of the scripts

--- js/script.js
+++ js/script.js
@@ -1,4 +1,28 @@
-var century, year, month, date;
+var date = document.getElementById('date').value;
+  
+var month = document.getElementById('month').value;
+
+var yearOfBirth = document.getElementById('year').value;
+
+
+var century = yearOfBirth.slice(0,2);
+var year = yearOfBirth.slice(2,4);
+
+var genders = document.getElementById("gender");
+var gender = genders.options[genders.selectedIndex].text;
+//A function to verify
+function verify(){
+  if(document.getElementById('date').value===""){
+    alert("You must enter a value");
+  
+  }else if(document.getElementById('month').value===""){
+    alert("You must enter a value");
+  }else if(document.getElementById('year').value===""){
+    alert("You must enter a value");
+  }else{
+    return false;
+  }
+}
 
 function generateName(){
   var date = document.getElementById('date').value;
@@ -22,10 +46,10 @@ function generateName(){
   var dayOfBirth = Math.floor(dayOfBirth);
 
   if (gender === "Male") {
-       document.getElementById("result").innerHTML = "You were born on" + dayOfWeek[dayOfBirth - 1]+ "and your name is" +  maleName[dayOfBirth -1];
+       document.getElementById("result").innerHTML = "You were born on " + dayOfWeek[dayOfBirth - 1]+ " and your name is " +  maleName[dayOfBirth -1];
        }
        else if (gender === "Female") {
-           document.getElementById("result").innerHTML = "You were born on" + dayOfWeek[dayOfBirth - 1]+ "and your name is" +  femaleName[dayOfBirth -1];
+           document.getElementById("result").innerHTML = "You were born on " + dayOfWeek[dayOfBirth - 1]+ " and your name is " +  femaleName[dayOfBirth -1];
        }
     
 }
