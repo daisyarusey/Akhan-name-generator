787046043da974aa2dd0be17d665b161a52a0927
daisyarusey <daisyarusey.dc@gmail.com>
2019-07-14 17:32:57 +0300

    Edit the function

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 var century, year, month, date;
 
-document.getElementById('theform').onsubmit = function generateName(){
-  console.log(document.getElementById('theform').value);
+function generateName(){
   var date = document.getElementById('date').value;
   
   var month = document.getElementById('month').value;
@@ -9,24 +8,24 @@ document.getElementById('theform').onsubmit = function generateName(){
   var yearOfBirth = document.getElementById('year').value;
   
 
-  var century = parseInt(yearOfBirth.slice(0,2));
-  var year = parseInt(yearOfBirth.slice(2,4));
+  var century = yearOfBirth.slice(0,2);
+  var year = yearOfBirth.slice(2,4);
 
   var genders = document.getElementById("gender");
   var gender = genders.options[genders.selectedIndex].text;
-  
-  
+
   var maleName = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
   var femaleName = ["Akosu", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Amna"];
   var dayOfWeek = ["Sunday", "Monday", "Tuesday", "Wensday", "Thursday", "Friday", "Sunday"];
+
   var dayOfBirth = ( ( (century/4) -2*year-1) + ((5*year/4) ) + ((26*(month+1)/10)) + date ) % 7;
   var dayOfBirth = Math.floor(dayOfBirth);
 
   if (gender === "Male") {
-       document.getElementById("result").innerHTML = "You were born on" + dayOfWeek[dayOfBirth - 1] "and your name is" +  maleName[dayOfBirth -1];
+       document.getElementById("result").innerHTML = "You were born on" + dayOfWeek[dayOfBirth - 1]+ "and your name is" +  maleName[dayOfBirth -1];
        }
        else if (gender === "Female") {
-           document.getElementById("result").innerHTML = "You were born on" + dayOfWeek[dayOfBirth - 1] "and your name is" +  femaleName[dayOfBirth -1];
+           document.getElementById("result").innerHTML = "You were born on" + dayOfWeek[dayOfBirth - 1]+ "and your name is" +  femaleName[dayOfBirth -1];
        }
     
 }
