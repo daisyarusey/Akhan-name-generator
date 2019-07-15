d16a7b3694f24b36eed6117d635c73ec3862543c
daisyarusey <daisyarusey.dc@gmail.com>
2019-07-15 12:50:50 +0300

    Edit function verify

--- js/script.js
+++ js/script.js
@@ -1,13 +1,19 @@
 //A function to verify
 function verify(){
-  if(document.getElementById('date').value===""){
-    alert("You must enter a value");
-  
-  }else if(document.getElementById('month').value===""){
-    alert("You must enter a value");
-  }else if(document.getElementById('year').value===""){
-    alert("You must enter a value");
-  }else{
+  var date = document.getElementById('date').value;
+  var month = document.getElementById('month').value;
+  var yearOfBirth = document.getElementById('year').value;
+
+  if(date===""||date<1||date>31){
+    alert("Date must be between 1 and 31");
+  }
+  else if(month===""||month<1||month>12){
+    alert("Date must be between 1 and 12");
+  }
+  else if(year===""){
+    alert("You must enter a value year");
+  }
+  else{
     return false;
   }
 }
@@ -28,7 +34,7 @@ function generateName(){
 
   var maleName = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
   var femaleName = ["Akosu", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Amna"];
-  var dayOfWeek = ["Sunday", "Monday", "Tuesday", "Wensday", "Thursday", "Friday", "Sunday"];
+  var dayOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Sunday"];
 
   var dayOfBirth = ( ( (century/4) -2*year-1) + ((5*year/4) ) + ((26*(month+1)/10)) + date ) % 7;
   var dayOfBirth = Math.floor(dayOfBirth);
